fae2cea63cb485c2358c93e5b4f65ace44a21b7a
Granome <101628967+Granome@users.noreply.github.com>
2024-01-26 15:57:36 +0200

    Update 159387_task1.py

--- 159387_task1.py
+++ 159387_task1.py
@@ -1,3 +1,7 @@
+import numpy
+import matplotlib.pyplot as plt
+
+
 total_words = 0
 total_chars = 0
 n_of_lines = 0
@@ -19,3 +23,30 @@ avarage_chars = total_chars/n_of_lines
 
 print("The avarage number of words per line: ", avarage_words)
 print("The avarage number of chars per line: ", avarage_chars)
+
+def caculate_formula(a, b, x):
+    return a*x*x*x+b*x
+
+
+
+x = []
+for i in range(0, 200, 2):
+    x.append(i/100)
+
+column1 = []
+for i in x:
+    column1.append(caculate_formula(1, 2, i))
+
+column2 = []
+for i in x:
+    column1.append(caculate_formula(5, 5, i))
+
+y = numpy.array(column1, column2)
+
+print(y[0])
+
+plt.figure()
+plt.plot(x, y)
+plt.plot(x, y[1])
+
+plt.show()
